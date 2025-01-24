ff7df8446ec7e29a8411f7d879d9a479ad40fa62
Erik Sarriegui <92121483+ErikSarriegui@users.noreply.github.com>
2025-01-24 14:43:56 +0100

    train model

--- train.py
+++ train.py
@@ -9,16 +9,24 @@ DEVICE = 0 # GPU index
 OPTIMIZER = "AdamW"
 BATCH_SIZE = 16
 
-def train_model():
-    model = YOLO(YOLO_MODEL)
+def train_model(
+    data_yaml: str = DATA_YAML,
+    yolo_model: str = YOLO_MODEL,
+    epochs: int = EPOCHS,
+    imgsz: int = IMAGE_SIZE,
+    device: int = DEVICE,
+    optimizer: str = OPTIMIZER,
+    batch_size: int = BATCH_SIZE
+):
+    model = YOLO(yolo_model)
 
     results = model.train(
-        data = DATA_YAML,
-        epochs = EPOCHS,
-        imgsz = IMAGE_SIZE,
-        device = DEVICE,
-        optimizer = OPTIMIZER,
-        batch = BATCH_SIZE
+        data = data_yaml,
+        epochs = epochs,
+        imgsz = imgsz,
+        device = device,
+        optimizer = optimizer,
+        batch = batch_size
     )
 
 if __name__ == "__main__":
